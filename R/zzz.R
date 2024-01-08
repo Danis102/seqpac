c6efbbdea52ea9a3a221e18bf98b1622ddebf06d
lovisaor <64014344+lovisaor@users.noreply.github.com>
2024-01-08 14:31:41 +0100

    Update zzz.R
    refer to the accepted paper

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("Welcome to seqpac.", 
-                        "\nPlease, cite 'Skog et al. 2021, bioRxiv'")
+                        "\nPlease, cite 'Skog et al. 2023, Bioinformatics'")
 }
