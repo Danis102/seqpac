a1419dd1c6edc8d7c1a99705021493f38fc976d2
lovisaor <lovisa.orkenby@liu.se>
2026-03-23 11:44:27 +0100

    Removed ginnards::move_layers

--- R/PAC_pca.R
+++ R/PAC_pca.R
@@ -217,14 +217,6 @@ PAC_pca <- function(PAC, norm="counts", style="pheno", graphs=TRUE,
       grphs$PC2_PC3 <- do.call(
         factoextra::fviz_pca_ind,
         modifyList(base_args, c(list(axes = c(2, 3), title = "PC2_PC3 - Pheno"))))
-      
-      grphs <- lapply(grphs, function(x){
-        x <- gginnards::move_layers(x, match_type="GeomPoint",
-                                    position = "top")
-        x <- gginnards::move_layers(x, match_type="GeomTextRepel", 
-                                    position = "top")
-        return(x)
-        })
       }
     }
   
@@ -296,16 +288,6 @@ PAC_pca <- function(PAC, norm="counts", style="pheno", graphs=TRUE,
     grphs$PC2_PC3 <- do.call(
       factoextra::fviz_pca_ind,
       modifyList(base_args, c(list(axes = c(2, 3), title = "PC2_PC3 - Biplot"))))
-    
-    grphs <- lapply(grphs, function(x){
-      x <- gginnards::move_layers(x, match_type="GeomPoint", 
-                                  position = "bottom") 
-      x <- gginnards::move_layers(x, match_type="GeomArrow", 
-                                  position = "top")
-      x <- gginnards::move_layers(x, match_type="GeomTextRepel", 
-                                  position = "top")
-      return(x)
-      })
   }
   print(cowplot::plot_grid(plotlist=grphs, ncol=2, nrow=2))
   return(list(graphs=grphs, pca=pca_res))
